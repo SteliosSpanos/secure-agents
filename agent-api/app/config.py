85ab44fb84bc95008c6a8c5aeccb188085e123f3
SteliosSpanos <stylspan19@gmail.com>
2026-04-07 18:07:55 +0300

    Fixes settings to follow best practices

--- agent-api/app/config.py
+++ agent-api/app/config.py
@@ -1,13 +1,15 @@
-from pydantic_settings import BaseSettings
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 class Settings(BaseSettings):
-    aws_region: str = os.getenv("AWS_REGION", "eu-central-1")
-    s3_bucket_name: str = os.getenv("S3_BUCKET_NAME")
-    sqs_queue_url: str = os.getenv("SQS_QUEUE_URL")
+    aws_region: str = Field(default="eu-central-1")
+    s3_bucket_name: str = Field(...)
+    sqs_queue_url: str = Field(...)
 
-    class Config:
-        env_file = ".env"
+    max_file_size_mb: int = 10
+    api_key_header_name: str = "X-api-key"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 settings = Settings()
