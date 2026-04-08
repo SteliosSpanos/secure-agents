a900d1158a7641ea164d125280baae96d8768a5f
SteliosSpanos <stylspan19@gmail.com>
2026-04-08 18:16:11 +0300

    Creates the S3 backend bucket

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,1 +1,24 @@
 data "aws_caller_identity" "current" {}
+
+// S3 Bucket Policy (Remote Backend)
+
+data "aws_iam_policy_document" "state_force_ssl" {
+  statement {
+    sid    = "AllowSSLOnly"
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.terraform_state.arn,
+      "${aws_s3_bucket.terraform_state.arn}/*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
