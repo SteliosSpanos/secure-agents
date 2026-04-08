e84e7f6e5b4da7bc12c9689f53ef0670cfa49104
SteliosSpanos <stylspan19@gmail.com>
2026-04-08 13:00:14 +0300

    Adds dynamodb table name

--- agent-api/app/config.py
+++ agent-api/app/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     aws_region: str = Field(default="eu-central-1")
     s3_bucket_name: str = Field(...)
     sqs_queue_url: str = Field(...)
+    api_keys_table_name: str = Field(default="SecureAgents_APIKeys")
 
     max_file_size_mb: int = 10
     api_key_header_name: str = "X-api-key"
