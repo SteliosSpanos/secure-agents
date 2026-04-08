a7362a2aede511b81aa35c279bcf1edaeb390c07
SteliosSpanos <stylspan19@gmail.com>
2026-04-08 16:04:00 +0300

    Function to generate client key

---
+++ agent-api/client_key_script.py
@@ -0,0 +1,17 @@
+import secrets
+import hashlib
+
+def generate_client_key(client_name: str):
+    raw_key = "ak_live_" + secrets.token_urlsafe(32)
+
+    hashed_key = hashlib.sha256(raw_key.encode("utf-8")).hexdigest()
+
+    print("=== SAVING TO DYNAMODB ===")
+    print(f"Client ID: {client_name}")
+    print(f"Stored Hash: {hashed_key}\n")
+
+    print("=== SEND TO CLIENT ===")
+    print(f"Raw API key: {raw_key}")
+    
+
+generate_client_key("client_omega")
