7ca6baf627dbaa174d73dbc3dd9aa293512a10dc
SteliosSpanos <stylspan19@gmail.com>
2026-04-09 16:21:55 +0300

    Includes the CIDR blocks needed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,27 @@ variable "project_name" {
   default     = "agents"
   description = "Project name for resource naming"
 }
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR block"
+}
+
+variable "public_subnet_cidr" {
+  type        = string
+  default     = "10.0.1.0/24"
+  description = "Public subnet CIDR block"
+}
+
+variable "private_subnet_1_cidr" {
+  type        = string
+  default     = "10.0.2.0/24"
+  description = "Private subnet 1 CIDR block"
+}
+
+variable "private_subnet_2_cidr" {
+  type        = string
+  default     = "10.0.3.0/24"
+  description = "Private subnet 2 CIDR"
+}
