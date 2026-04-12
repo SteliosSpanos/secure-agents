8a42f94b0ad11963a550308a14c7fd765cd0dbc5
SteliosSpanos <stylspan19@gmail.com>
2026-04-12 21:00:25 +0300

    Moves VPC flow logs iam in this file

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -52,3 +52,17 @@ resource "aws_iam_role_policy" "authorizer_policy" {
   role   = aws_iam_role.authorizer_role.id
   policy = data.aws_iam_policy_document.authorizer_iam_policy.json
 }
+
+// VPC Flow Logs
+
+resource "aws_iam_role" "vpc_flow_log" {
+  name               = "${var.project_name}-vpc-flow-log-role"
+  assume_role_policy = data.aws_iam_policy_document.vpc_flow_log_assume_role.json
+}
+
+resource "aws_iam_role_policy" "vpc_flow_log" {
+  name = "${var.project_name}-vpc-flow-log-policy"
+  role = aws_iam_role.vpc_flow_log.id
+
+  policy = data.aws_iam_policy_document.vpc_flow_log.json
+}
