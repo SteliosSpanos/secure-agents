44f1c4c814f2be47ec3492a5d243812d691dffd2
SteliosSpanos <stylspan19@gmail.com>
2026-04-17 23:20:04 +0300

    Adds TTL for the api keys table

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -25,6 +25,11 @@ resource "aws_dynamodb_table" "api_keys" {
     enabled = true
   }
 
+  ttl {
+    attribute_name = "expires_at"
+    enabled        = true
+  }
+
   tags = {
     Name = "${var.project_name}-api-keys"
   }
