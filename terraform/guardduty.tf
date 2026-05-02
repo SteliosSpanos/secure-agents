575e66417c03d127387da6c6a775bdeb63563edc
SteliosSpanos <stylspan19@gmail.com>
2026-05-02 16:45:22 +0300

    Changes to newer terraform syntax

--- terraform/guardduty.tf
+++ terraform/guardduty.tf
@@ -5,29 +5,33 @@
 resource "aws_guardduty_detector" "main" {
   enable = true
 
-  // S3 Protection
-  features {
-    name   = "S3_DATA_EVENTS"
-    status = "ENABLED"
-  }
-
-  // Fargate Runtime Monitoring
-  features {
-    name   = "RUNTIME_MONITORING"
-    status = "ENABLED"
-
-    additional_configuration {
-      name   = "EKS_ADDON_MANAGEMENT"
-      status = "DISABLED"
-    }
-
-    additional_configuration {
-      name   = "ECS_FARGATE_AGENT_MANAGEMENT"
-      status = "ENABLED"
-    }
-  }
-
   tags = {
     Name = "${var.project_name}-guardduty"
+  }
+}
+
+// S3 Protection
+
+resource "aws_guardduty_detector_feature" "s3_logs" {
+  detector_id = aws_guardduty_detector.main.id
+  name        = "S3_DATA_EVENTS"
+  status      = "ENABLED"
+}
+
+// Fargate Runtime Monitoring
+
+resource "aws_guardduty_detector_feature" "runtime_monitoring" {
+  detector_id = aws_guardduty_detector.main.id
+  name        = "RUNTIME_MONITORING"
+  status      = "ENABLED"
+
+  additional_configuration {
+    name   = "EKS_ADDON_MONITORING"
+    status = "DISABLED"
+  }
+
+  additional_configuration {
+    name   = "ECS_FARGATE_AGENT_MANAGEMENT"
+    status = "ENABLED"
   }
 }
