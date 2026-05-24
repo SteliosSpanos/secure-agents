d717a200e647fbbc6b8743ebc736d74990c42683
SteliosSpanos <stylspan19@gmail.com>
2026-05-24 17:38:01 +0300

    Creates SGs for the EC2 instances

--- terraform/security.tf
+++ terraform/security.tf
@@ -222,3 +222,86 @@ resource "aws_security_group" "webhook_consumer_sg" {
     Name = "${var.project_name}-webhook-consumer-sg"
   }
 }
+
+// EC2 Jump Box
+
+resource "aws_security_group" "jump_box" {
+  name        = "${var.project_name}-jump-box-sg"
+  description = "Security group for jump box instance"
+  vpc_id      = aws_vpc.agents_vpc.id
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${data.external.my_ip.result.ip}/32"]
+  }
+
+  ingress {
+    description = "ICMP (ping) from my IP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["${data.external.my_ip.result.ip}/32"]
+  }
+
+  egress {
+    description = "All outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-jump-box-sg"
+  }
+}
+
+// EC2 NAT Instance
+
+resource "aws_security_group" "nat_instance" {
+  name        = "${var.project_name}-nat-instance-sg"
+  description = "Security group for NAT instance"
+  vpc_id      = aws_vpc.agents_vpc.id
+
+  ingress {
+    description = "HTTPS from private subnets"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [
+      aws_subnet.agents_private_subnet_1.cidr_block,
+      aws_subnet.agents_private_subnet_2.cidr_block
+    ]
+  }
+
+  ingress {
+    description     = "SSH from jump box"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.jump_box.id]
+  }
+
+  ingress {
+    description     = "ICMP (ping) from jump box"
+    from_port       = -1
+    to_port         = -1
+    protocol        = "icmp"
+    security_groups = [aws_security_group.jump_box.id]
+  }
+
+  egress {
+    description = "All outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-nat-instance-sg"
+  }
+}
