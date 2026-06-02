c48a6f425a6c0193e65de1849ac345736549f8f3
SteliosSpanos <stylspan19@gmail.com>
2026-06-02 16:27:45 +0300

    Adds ecs exec role in shared kms usage

--- terraform/data.tf
+++ terraform/data.tf
@@ -401,7 +401,8 @@ data "aws_iam_policy_document" "jobs_table_kms_policy" {
         aws_iam_role.api_task_role.arn,
         aws_iam_role.agent_task_role.arn,
         aws_iam_role.webhook_trigger_role.arn,
-        aws_iam_role.webhook_consumer_role.arn
+        aws_iam_role.webhook_consumer_role.arn,
+        aws_iam_role.ecs_execution_role.arn
       ]
     }
     actions = [
