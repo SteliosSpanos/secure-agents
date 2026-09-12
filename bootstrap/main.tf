79dc9464ebbab329b535084eb6e5a804df2f9c2e
SteliosSpanos <stylspan19@gmail.com>
2026-09-12 13:57:10 +0300

    feat: add prevent destroy for bucket

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -65,11 +65,11 @@ data "aws_iam_policy_document" "state_force_ssl" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = "${var.project_name}-terraform-state-${data.aws_caller_identity.current.account_id}"
-  force_destroy = true
+  force_destroy = false
 
-  /*lifecycle {
+  lifecycle {
     prevent_destroy = true
-  }*/
+  }
 
   tags = {
     Name = "${var.project_name}-terraform-state"
